a73b744e4ac042539b65960b04c38db6ff41b41d
ciathefed <ciathefed@protonmail.com>
2025-09-09 19:20:46 -0400

    fix(call): place variadic marker at correct position in IR output

--- pkg/qbe/call.go
+++ pkg/qbe/call.go
@@ -12,11 +12,12 @@ type arg struct {
 
 // A CallInst represents a call instruction.
 type CallInst struct {
-	dest     *dest      // Where the result of the call is stored, nil if no result.
-	env      *Temporary // The env parameter, nil if not passed.
-	variadic bool       // Set to true if the function called is variadic. Defaults to false
-	target   Value
-	args     []arg
+	dest         *dest      // Where the result of the call is stored, nil if no result.
+	env          *Temporary // The env parameter, nil if not passed.
+	variadic     bool       // Set to true if the function called is variadic. Defaults to false
+	variadicFrom int        // Index where variadic arguments start (NEW FIELD)
+	target       Value
+	args         []arg
 }
 
 func (*CallInst) isInst() {}
@@ -36,17 +37,36 @@ func (c *CallInst) String() string {
 	if c.env != nil {
 		builder.WriteString("env ")
 		builder.WriteString(c.env.String())
-		builder.WriteString(", ")
+		if len(c.args) > 0 || c.variadic {
+			builder.WriteString(", ")
+		}
 	}
-	for _, arg := range c.args {
+
+	// Write arguments with variadic marker at the correct position
+	for i, arg := range c.args {
+		// Insert variadic marker before variadic arguments
+		if c.variadic && i == c.variadicFrom {
+			builder.WriteString("..., ")
+		}
+
 		builder.WriteString(fmt.Sprint(arg.Type.Name()))
 		builder.WriteByte(' ')
 		builder.WriteString(fmt.Sprint(arg.Value))
-		builder.WriteString(", ")
+
+		// Add comma if not the last argument
+		if i < len(c.args)-1 {
+			builder.WriteString(", ")
+		}
 	}
-	if c.variadic {
+
+	// Handle case where there are no variadic args but function is marked variadic
+	if c.variadic && c.variadicFrom >= len(c.args) {
+		if len(c.args) > 0 {
+			builder.WriteString(", ")
+		}
 		builder.WriteString("...")
 	}
+
 	builder.WriteByte(')')
 	return builder.String()
 }
@@ -59,11 +79,12 @@ func (b *Block) InsertCall(target Value) *CallInst {
 		panic("target of call instruction cannot be nil")
 	}
 	inst := &CallInst{
-		dest:     nil,
-		env:      nil,
-		variadic: false,
-		target:   target,
-		args:     nil,
+		dest:         nil,
+		env:          nil,
+		variadic:     false,
+		variadicFrom: 0,
+		target:       target,
+		args:         nil,
 	}
 	b.insertInstruction(inst)
 	return b.insts[len(b.insts)-1].(*CallInst)
@@ -82,9 +103,16 @@ func (c *CallInst) SetEnv(env Temporary) {
 	c.env = &env
 }
 
-// SetVariadic sets c as variadic.
+// SetVariadic sets c as variadic with variadic arguments starting from the current argument count.
 func (c *CallInst) SetVariadic() {
 	c.variadic = true
+	c.variadicFrom = len(c.args)
+}
+
+// SetVariadicFrom sets c as variadic with variadic arguments starting from the specified index.
+func (c *CallInst) SetVariadicFrom(index int) {
+	c.variadic = true
+	c.variadicFrom = index
 }
 
 // InsertArg adds argument value with type type_ to the end of the argument list of c.
